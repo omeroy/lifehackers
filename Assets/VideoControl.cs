8da9eb17ea4b4a92a0a04b6036ebd19d3db9884c
zhenhaoli <zhenhao.li@outlook.com>
2016-11-19 23:02:51 +0100

    First commit

---
+++ Assets/VideoControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoControl : MonoBehaviour {
+
+    private MoviePlayerSample player;
+    private int button = 0;
+    // Use this for initialization
+    void Start () {
+         player = GetComponent<MoviePlayerSample>();
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(button)) { 
+            player.SetPaused(!player.videoPaused);
+        } 
+
+    }
+}
